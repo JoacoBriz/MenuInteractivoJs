e24705dec4b7bc8bdff32290f31c61b57a61c578
Joaquin Brizuela <joaquinbriz1@gmail.com>
2021-02-10 18:32:21 -0300

    quitando bienvenida

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
-//Bienvenida 
+// //Bienvenida 
 
-var nombre = prompt("Bienvenido! Cómo te llamas?");
-function saludo() {
-  var mensajeSaludo = `Un gusto tenerte aqui ${nombre}, hora de elegir que comer!`;
-  alert(mensajeSaludo);
-}
-saludo()
+// var nombre = prompt("Bienvenido! Cómo te llamas?");
+// function saludo() {
+//   var mensajeSaludo = `Un gusto tenerte aqui ${nombre}, hora de elegir que comer!`;
+//   alert(mensajeSaludo);
+// }
+// saludo()
 
 
 // showProductos(productosGlobal);
@@ -69,31 +69,31 @@ function ordenarPlato (agregarPlato)  {
 
 
 
-//Productos
-var carrito = []; 
-var cantidad = 0;
+// //Productos
+// var carrito = []; 
+// var cantidad = 0;
 
-function Productos(id, nombre, precio) {
-  this.id = id;
-  this.nombre = nombre;
-  this.precio = precio;
-}
+// function Productos(id, nombre, precio) {
+//   this.id = id;
+//   this.nombre = nombre;
+//   this.precio = precio;
+// }
 
-var product1 = new Productos (0, "American Coffe", "$3,5");
-var product2 = new Productos (1, "Ham and Cheese Toast", "$4");
-var product3 = new Productos (2, "Mocha Frappuccino", "$4,5");
-var product4 = new Productos (3, "Espresso Brownie", "$2,5");
+// var product1 = new Productos (0, "American Coffe", "$3,5");
+// var product2 = new Productos (1, "Ham and Cheese Toast", "$4");
+// var product3 = new Productos (2, "Mocha Frappuccino", "$4,5");
+// var product4 = new Productos (3, "Espresso Brownie", "$2,5");
 
-function agregarCarrito(id) {
-  if (cantidad < 10) {
-    carrito.push(Productos[id]);
-    cantidad = carrito.length;
-    alert(`Agregaste un ${nombre}`);
-    alert(`Llevas ${cantidad} en tu orden`);
-  } else {
-    alert("Llegaste al limite de pedidos");
-  }
-}
+// function agregarCarrito(id) {
+//   if (cantidad < 10) {
+//     carrito.push(Productos[id]);
+//     cantidad = carrito.length;
+//     alert(`Agregaste un ${nombre}`);
+//     alert(`Llevas ${cantidad} en tu orden`);
+//   } else {
+//     alert("Llegaste al limite de pedidos");
+//   }
+// }
 
-agregarCarrito()
+// agregarCarrito()
 
