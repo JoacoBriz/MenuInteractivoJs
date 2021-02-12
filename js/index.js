660343e47b232a0a758ac07625891db2795a9cc1
Joaquin Brizuela <joaquinbriz1@gmail.com>
2021-02-11 21:30:51 -0300

    segunda entrega del PF

--- js/index.js
+++ js/index.js
@@ -1,12 +1,51 @@
-$( document ).ready(function () {
-  $(".buttonOrder").click(function() {
-    $(".buttonOrder").hide();
-    alert("Has ordenado un Producto!");
-    $(".buttonCancel").show();
-  })
-  $(".buttonCancel").click(function() {
-    $(".buttonCancel").hide();
-    alert("Has cancelado la orden");
-    $(".buttonOrder").show();
-  })
+
+//Cambio de Boton
+window.onload = function () {
+  const buttons = document.querySelectorAll(".buttonOrder");
+  buttons.forEach((btn) => {
+    btn.addEventListener("click", (event) => {
+      if (btn.innerHTML === "Ordenar") {
+        btn.innerHTML = "Cancelar";
+      }});
+  });
+};
+
+
+
+
+
+
+
+
+
+
+//Carrito de compras
+const addToShoppingCardButtons = document.querySelectorAll(".buttonOrder");
+addToShoppingCardButtons.forEach(addToCardButton => {
+  addToCardButton.addEventListener("click", addToCardClicked);
 })
+
+const shoppingCardItemsContainer =document.querySelector(".desayuno");
+
+//Agregar productos al carrito
+function addToCardClicked (event) {
+  const button =event.target;
+  const item = button.closest(".productoDesayuno");
+  
+  
+  const itemName = item.querySelector(".nombreProducto").textContent;
+  const itemPrice = item.querySelector(".precioProducto").textContent;
+
+  addItemToShoppingCard(itemName,itemPrice);
+}
+
+function addItemToShoppingCard (itemName,itemPrice) {
+  alert(`Has ordenado un ${itemName} por ${itemPrice}`);
+
+  searchFinalPrice ()
+}
+
+//Obtener Precio final -- No llegue con el tiempo :(
+function searchFinalPrice () {
+  let total = 0;
+}
