371477d7a1a3a27cd2dd01fa03c12e908f80f15e
Joaquin Brizuela <joaquinbriz1@gmail.com>
2021-02-13 14:27:16 -0300

    .

--- js/index.js
+++ js/index.js
@@ -13,24 +13,18 @@ window.onload = function () {
 
 
 
-
-
-
-
-
-
 //Carrito de compras
 const addToShoppingCardButtons = document.querySelectorAll(".buttonOrder");
 addToShoppingCardButtons.forEach(addToCardButton => {
   addToCardButton.addEventListener("click", addToCardClicked);
 })
 
-const shoppingCardItemsContainer =document.querySelector(".desayuno");
+const shoppingCardItemsContainer =document.querySelector(".menuGlobal");
 
 //Agregar productos al carrito
 function addToCardClicked (event) {
   const button =event.target;
-  const item = button.closest(".productoDesayuno");
+  const item = button.closest(".productoGlobal");
   
   
   const itemName = item.querySelector(".nombreProducto").textContent;
