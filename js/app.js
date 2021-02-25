5e1bcc692ae5160c5a9bf253116d3300c8b798f7
Joaquin Brizuela <joaquinbriz1@gmail.com>
2021-02-25 16:44:18 -0300

    cantidadproductos

--- js/app.js
+++ js/app.js
@@ -1,8 +1,6 @@
 // Cambio de Boton con jQuery
 $(document).ready( function () {
-
-
-    //Cambio de boton
+  //Cambio de boton
   $(".buttonOrder").click(function (event) {
     if (this.innerHTML === "Ordenar") {
       agregarAlCarritoClick(event);
@@ -33,12 +31,12 @@ $(document).ready( function () {
         confirmButton: "alertCarritoButton",
         showCancelButton: "alertCarritoButtonCancel"
       }
-    });
-
-    //Cantidad del boton carrito
-    
-  })
-})
+      })
+    while(carrito.length > 0) {
+      carrito.pop();
+    }
+  });
+});
 
 
 //Carrito de compras
@@ -48,78 +46,76 @@ console.log(carrito.length);
 //Agregar productos al carrito (funciona)
 
 function agregarAlCarritoClick (event) {
-  const button =event.target;
-  const item = button.closest(".productoGlobal");
-  
-  const productoNombre = item.querySelector(".nombreProducto").textContent;
-  const productoPrecio = item.querySelector(".precioProducto").textContent; // Este esta devolviendo en texto
-  
-  agregarProductoAlCarrito(productoNombre,productoPrecio);
+const button =event.target;
+const item = button.closest(".productoGlobal");
+
+const productoNombre = item.querySelector(".nombreProducto").textContent;
+const productoPrecio = item.querySelector(".precioProducto").textContent; // Este esta devolviendo en texto
+
+agregarProductoAlCarrito(productoNombre,productoPrecio);
 }
 
 function agregarProductoAlCarrito (productoNombre, productoPrecio) {
-  const confirmation = Swal.fire({
-    icon: "success",
-    title:`Has ordenado un`,
-    html: `${productoNombre} por ${productoPrecio}`,
-    confirmButtonText: "Genial",
+const confirmation = Swal.fire({
+  icon: "success",
+  title:`Has ordenado un`,
+  html: `${productoNombre} por ${productoPrecio}`,
+  confirmButtonText: "Genial",
 
-    customClass: {
-      container: "alertAgregarCarrito",
-      title:"alertAgregarCarritoTitle",
-      html: "alertAgregarCarritoTexto",
-      confirmButton: "alertAgregarCarritoButton"
-    }
-  });
-
-  if (confirmation) {
-    productoPrecio = parseInt(productoPrecio.slice(1))
-    carrito.push({nombre: productoNombre, precio: productoPrecio});
-    precioFinal();
-    console.log(carrito.length);
-    var cantidadSumar = carrito.length;
-    const mostrarCantidad = document.querySelector(".carrito-cantidad-numero").innerHTML = cantidadSumar;
+  customClass: {
+    container: "alertAgregarCarrito",
+    title:"alertAgregarCarritoTitle",
+    html: "alertAgregarCarritoTexto",
+    confirmButton: "alertAgregarCarritoButton"
   }
+});
+
+if (confirmation) {
+  productoPrecio = parseInt(productoPrecio.slice(1))
+  carrito.push({nombre: productoNombre, precio: productoPrecio});
+  precioFinal();
+  var cantidadSumar = carrito.length;
+  const mostrarCantidad = document.querySelector(".carrito-cantidad-numero").innerHTML = cantidadSumar;
+}
 }
 
 // Quitar productos al carrito (Funciona)
 
 function quitarProductoDelCarritoClick (event) {
-  const button =event.target;
-  const item = button.closest(".productoGlobal");
-  
-  const nombreProducto = item.querySelector(".nombreProducto").textContent;
+const button =event.target;
+const item = button.closest(".productoGlobal");
 
-  quitarDelCarrito(nombreProducto);
+const nombreProducto = item.querySelector(".nombreProducto").textContent;
+
+quitarDelCarrito(nombreProducto);
 }
 
 function quitarDelCarrito (nombreProducto) {
-  const confirmation = Swal.fire({
-    icon: "success",
-    title: "Has quitado el",
-    html: `${nombreProducto} de tu orden`,
-    confirmButtonText: "Que pena",
+const confirmation = Swal.fire({
+  icon: "success",
+  title: "Has quitado el",
+  html: `${nombreProducto} de tu orden`,
+  confirmButtonText: "Que pena",
 
-    customClass: {
-      container: "alertQuitarCarrito",
-      title:"alertQuitarCarritoTitle",
-      html: "alertQuitarCarritoTexto",
-      confirmButton: "alertQuitarCarritoButton"
-    }
-  });
-  if (confirmation) {
-    carrito = carrito.filter(el => el.nombre != nombreProducto);
-    var cantidadRestar = carrito.length;
-    const mostrarCantidad = document.querySelector(".carrito-cantidad-numero").innerHTML = cantidadRestar;
-
+  customClass: {
+    container: "alertQuitarCarrito",
+    title:"alertQuitarCarritoTitle",
+    html: "alertQuitarCarritoTexto",
+    confirmButton: "alertQuitarCarritoButton"
   }
+});
+if (confirmation) {
+  carrito = carrito.filter(el => el.nombre != nombreProducto);
+  var cantidadRestar = carrito.length;
+  const mostrarCantidad = document.querySelector(".carrito-cantidad-numero").innerHTML = cantidadRestar;
+}
 }
 
 //Mostrar Precio final
 function precioFinal () {
-  let total = 0; 
-  carrito.forEach(el => total += el.precio);
-  return total;
+let total = 0; 
+carrito.forEach(el => total += el.precio);
+return total;
 }
 
 //Cambiar cantidad de productos
